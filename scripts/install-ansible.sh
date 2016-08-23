9fee6c8ec13f5d10db851f265cf21f1216a4bb85
Ron Kurr <kurr@kurron.org>
2016-08-22 20:00:17 -0400

    Upgrade to current Vagrant box

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -7,9 +7,15 @@ if [ -f "${DONEFILE}" ]; then
     exit 0
 fi
 
-sudo apt-get install -y libssl-dev libffi-dev
-sudo easy_install pip
-sudo pip install ansible
+$sudo apt-get install -y libssl-dev libffi-dev
+$sudo easy_install pip
+$sudo pip install ansible
+
+# supposedly, this is the newer way to install pip
+sudo apt-get install -y python-pip python-dev build-essential libssl-dev libffi-dev
+sudo pip install --upgrade pip
+sudo pip install --upgrade ansible
+#sudo pip install --upgrade boto
 
 # signal a successful provision
 touch ${DONEFILE}
